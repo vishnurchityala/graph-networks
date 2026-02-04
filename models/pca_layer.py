29d86e717df86f6b5a9203873ce7787e5054a5b9
vishnurchityala <103362352+vishnurchityala@users.noreply.github.com>
2026-02-04 13:13:15 +0530

    PCA Weights Generated

---
+++ models/pca_layer.py
@@ -0,0 +1,15 @@
+import torch
+from torch import nn
+
+class PCALayer(nn.Module):
+    def __init__(self, mean, components):
+        super().__init__()
+        mean = torch.tensor(mean, dtype=torch.float32)
+        components = torch.tensor(components, dtype=torch.float32)
+        self.register_buffer("mean", mean)
+        self.register_buffer("weight", components)
+
+    def forward(self, x):
+        # x: (B, D)
+        x = x - self.mean
+        return torch.matmul(x, self.weight.T)  # (B, K)
